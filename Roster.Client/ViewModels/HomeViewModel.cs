fadbc61ac0562a2482900c85bd8770c59eca585b
Jörg Pichler <joerg.pichler@gmail.com>
2020-06-29 18:44:35 +0200

    Completed module

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -6,7 +6,22 @@ namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
-        public string Title { get; set; } = "Roster App";
+        private string _title = "Roster App";
+
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(() => Title = "Roster App (v2.0)");
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
 
         public Command UpdateApplicationCommand { get; }
 
